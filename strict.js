faa8ddd2b547bcf3d1e1cbf813c4313e4f347bce
shamikhquraan1 <shamikhq@yahoo.com>
2021-07-25 07:52:38 +0300

    edditing

--- strict.js
+++ strict.js
@@ -30,8 +30,13 @@ let thE1=document.createElement('th');
 thE1.textContent='LOGO';
 trEl.appendChild(thE1);
 
+
+let thE68=document.createElement('th');
+thE68.textContent='Car Name';
+trEl.appendChild(thE68);
+
 let thE2=document.createElement('th');
-thE2.textContent='Car Name';
+thE2.textContent='Car model';
 trEl.appendChild(thE2);
 
 
@@ -43,26 +48,32 @@ tableEl.appendChild(trEl);
 tableContainer.appendChild.tableEl;
 
 let count =0;
+
+
 car.prototype.render=function(){
 
 let trEl=document.createElement('tr');
 let tdE5 =document.createElement('td');
 let aEl =document.createElement('a');
-aEl.innerHTML=`<ion-icon id=${count} name="close-circle-outline"></ion-icon>`;
+aEl.innerHTML=`<ion-icon id=${count} width="10px" height="10px" name="close-circle-outline"></ion-icon>`;
 aEl.addEventListener('click', cleaRow);
 tdE5.appendChild(aEl);
 trEl.appendChild(tdE5);
 
+
 count++;
 let displayImage=document.createElement('img');
 displayImage.setAttribute('src', this.image);
-displayImage.setAttribute('width', '80px');
-displayImage.setAttribute('height','40px');
+displayImage.setAttribute('width', '60px');
+displayImage.setAttribute('height','30px');
 
 let tdE2 =document.createElement('td');
 tdE2.appendChild(displayImage);
 trEl.appendChild(tdE2);
 
+let tdE66=document.createElement('td');
+tdE66.textContent=this.cName;
+trEl.appendChild(tdE66);
 
 let tdE1=document.createElement('td');
 tdE1.textContent=this.cModel;
